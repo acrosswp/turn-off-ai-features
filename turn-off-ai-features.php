92ce152b6d947936cad085053180bec22a031bb7
raftaar1191 <raftaar1191@gmail.com>
2026-04-09 02:44:14 +0530

    Update version to 0.0.4

--- turn-off-ai-features.php
+++ turn-off-ai-features.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Turn Off AI Features
  * Description: Adds an option to the General Settings page to turn off AI features in WordPress.
- * Version:     0.0.1
+ * Version:     0.0.4
  * Requires at least: 7.0
  * Requires PHP:      7.4
  * Author:      raftaar1191
